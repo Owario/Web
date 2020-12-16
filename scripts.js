8b0142ab7917ad385d077f2144b9c7266d13eda1
Salihov Marat <dastan0109@bk.ru>
2020-12-16 04:34:34 +0300

    front

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
+$(document).ready(
+    function()
+    {
+    $("#butt1").click(function(){
+      $("ol.dropbox").append("<li>Appended item</li>");
+    });
+});
 
-$( "p" ).click(function() {
-    $( this ).slideUp();
-})
+
+window.onload = function(){
+    var txt = window.location;
+    alert(document.documentURI);
+    if (txt===)
+    {
+        if (localStorage.getItem('token')===null)
+        {
+            alert("Войдите в аккаунт!");
+        }
+    }
+}
